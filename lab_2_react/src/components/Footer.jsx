d6b095d7af570f3a99dd3c1b984ebc8f8f15dfd4
Pavlo Hada <your_email@gmail.com>
2026-03-09 11:24:55 +0200

    Added GitHub link to CV

--- lab_2_react/src/components/Footer.jsx
+++ lab_2_react/src/components/Footer.jsx
@@ -2,8 +2,14 @@ function Footer() {
   return (
     <footer>
       <h2>Контакти</h2>
-      <p>Email: pavlo@gmail.com</p>
+      <p>Електронна пошта: pavlo@gmail.com</p>
       <p>Телефон: +380632532487</p>
+      <p>
+        GitHub: 
+        <a href="https://github.com/Swpavlo/lab_2" target="_blank">
+          https://github.com/Swpavlo/lab_2
+        </a>
+      </p>
     </footer>
   );
 }
